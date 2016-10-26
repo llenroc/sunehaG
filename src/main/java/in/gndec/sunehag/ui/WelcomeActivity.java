cdcf4a6e4f9a8b944c4e26ac16a17e19b6b4f2ee
amarjeet <amarjeetkapoor1>
2016-10-27 05:22:13 +0530

    Adds splash screen to the app and adds configure file in gitignore

--- src/main/java/in/gndec/sunehag/ui/WelcomeActivity.java
+++ src/main/java/in/gndec/sunehag/ui/WelcomeActivity.java
@@ -9,38 +9,15 @@ import android.view.View;
 import android.widget.Button;
 
 import in.gndec.sunehag.R;
+import in.gndec.sunehag.entities.Conversation;
 
 public class WelcomeActivity extends Activity {
 
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
-		if (getResources().getBoolean(R.bool.portrait_only)) {
-			setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		}
-		final ActionBar ab = getActionBar();
-		if (ab != null) {
-			ab.setDisplayShowHomeEnabled(false);
-			ab.setDisplayHomeAsUpEnabled(false);
-		}
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.welcome);
-		final Button createAccount = (Button) findViewById(R.id.create_account);
-		createAccount.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(WelcomeActivity.this, MagicCreateActivity.class);
-				intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-				startActivity(intent);
-			}
-		});
-		final Button useOwnProvider = (Button) findViewById(R.id.use_own_provider);
-		useOwnProvider.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				startActivity(new Intent(WelcomeActivity.this, EditAccountActivity.class));
-			}
-		});
-
+		startActivity(new Intent(WelcomeActivity.this, ConversationActivity.class));
+		finish();
 	}
 
 }
